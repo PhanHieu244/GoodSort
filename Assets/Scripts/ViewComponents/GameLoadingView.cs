69b0d13ff48bbd8829d18f96c2ddd43761bfb4b6
pro_c <hieutg244@gmail.com>
2025-04-03 22:48:10 +0700

    init project

--- Assets/Scripts/ViewComponents/GameLoadingView.cs
+++ Assets/Scripts/ViewComponents/GameLoadingView.cs
@@ -5,59 +5,47 @@ using DG.Tweening;
 
 public class GameLoadingView : BaseView
 {
-    public RectTransform bgLeft;
+	public RectTransform bgLeft;
 
-    public RectTransform bgRight;
+	public RectTransform bgRight;
 
-    public RectTransform titleLeft;
+	public RectTransform titleLeft;
 
-    public RectTransform titleRight;
+	public RectTransform titleRight;
+	public override void InitView() { }
+	
+	public override void Start() { }
 
-    public override void InitView()
-    {
+	public override void Update() { }
 
-    }
+	public override void ShowView()
+	{
 
-    public override void Start()
-    {
+		canvasGroup.alpha=1.0f;
 
-    }
+		bgLeft.localPosition    =new Vector3(-240,0,0);
+		bgRight.localPosition   =new Vector3(240,0,0);
+		titleLeft.localPosition =new Vector3(0,0,0);
+		titleRight.localPosition=new Vector3(0,0,0);
+		AudioManager.instance.loadingGameSound.Play();
 
-    public override void Update()
-    {
 
-    }
+		bgLeft.DOLocalMoveX(-400,1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear);
+		bgRight.DOLocalMoveX(400,1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear);
+		titleLeft.DOLocalMoveX(-400,1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear);
+		titleRight.DOLocalMoveX(400,1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear).OnComplete
+		(
+		() =>
+		{
+			HideView();
+			bgLeft.localPosition    =new Vector3(-240,0,0);
+			bgRight.localPosition   =new Vector3(240,0,0);
+			titleLeft.localPosition =new Vector3(0,0,0);
+			titleRight.localPosition=new Vector3(0,0,0);
 
-    public override void ShowView()
-    {
+		}
+		);
+	}
 
-        canvasGroup.alpha = 1.0f;
-
-        bgLeft.localPosition = new Vector3(-240, 0, 0);
-        bgRight.localPosition = new Vector3(240, 0, 0);
-        titleLeft.localPosition = new Vector3(0, 0, 0);
-        titleRight.localPosition = new Vector3(0, 0, 0);
-        AudioManager.instance.loadingGameSound.Play();
-
-        bgLeft.DOLocalMoveX(-400, 1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear);
-        bgRight.DOLocalMoveX(400, 1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear);
-        titleLeft.DOLocalMoveX(-400, 1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear);
-        titleRight.DOLocalMoveX(400, 1f).SetRelative(true).SetDelay(0.5f).SetEase(Ease.Linear).OnComplete
-            (
-            () =>
-            {
-                HideView();
-                bgLeft.localPosition = new Vector3(-240, 0, 0);
-                bgRight.localPosition = new Vector3(240, 0, 0);
-                titleLeft.localPosition = new Vector3(0, 0, 0);
-                titleRight.localPosition = new Vector3(0, 0, 0);
-               
-            }
-            );
-    }
-
-    public override void HideView()
-    {
-        canvasGroup.alpha = 0.0f;
-    }
+	public override void HideView() { canvasGroup.alpha=0.0f; }
 }
