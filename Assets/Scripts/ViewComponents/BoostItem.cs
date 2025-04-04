7b31b769be11f370ca8489043f8fc53c28694554
pro_c <hieutg244@gmail.com>
2025-04-04 22:10:46 +0700

    update ui

--- Assets/Scripts/ViewComponents/BoostItem.cs
+++ Assets/Scripts/ViewComponents/BoostItem.cs
@@ -5,71 +5,62 @@ using UnityEngine.UI;
 
 public class BoostItem : MonoBehaviour
 {
-    public GameObject countObj;
+	public GameObject countObj;
 
-    public GameObject lockObj;
+	public GameObject lockObj;
 
-    public GameObject addObj;
+	public GameObject addObj;
 
-    public Text countText;
+	public Text countText;
 
-    public enum STATE
-    {
-        LOCK,
-        AVAILABLE,
-        UNAVAILABLE
-    }
+	public enum STATE
+	{
+		LOCK,
+		AVAILABLE,
+		UNAVAILABLE
+	}
 
-    public STATE currentState;
+	public STATE currentState;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start() { }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update() { }
 
-    public void SetLock()
-    {
-        currentState = STATE.LOCK;
+	public void SetLock()
+	{
+		currentState=STATE.LOCK;
 
-        lockObj.SetActive(true);
-        countObj.SetActive(false);
-        addObj.SetActive(false);
-    }
+		lockObj.SetActive(true);
+		countObj.SetActive(false);
+		addObj.SetActive(false);
+	}
 
-    public void UnLock()
-    {
-        currentState = STATE.AVAILABLE;
-        lockObj.SetActive(false);
-        countObj.SetActive(true);
-        addObj.SetActive(false);
-    }
+	public void UnLock()
+	{
+		currentState=STATE.AVAILABLE;
+		lockObj.SetActive(false);
+		countObj.SetActive(true);
+		addObj.SetActive(false);
+	}
 
-    public void SetUnAvailable()
-    {
-        currentState = STATE.UNAVAILABLE;
-        lockObj.SetActive(false);
-        countObj.SetActive(false);
-        addObj.SetActive(true);
-    }
+	public void SetUnAvailable()
+	{
+		currentState=STATE.UNAVAILABLE;
+		lockObj.SetActive(false);
+		countObj.SetActive(false);
+		addObj.SetActive(true);
+	}
 
-    public void SetAvailable(int count)
-    {
-        currentState = STATE.AVAILABLE;
-        lockObj.SetActive(false);
-        countObj.SetActive(true);
-        addObj.SetActive(false);
-        countText.text = count.ToString();
-    }
+	public void SetAvailable(int count)
+	{
+		currentState=STATE.AVAILABLE;
+		lockObj.SetActive(false);
+		countObj.SetActive(true);
+		addObj.SetActive(false);
+		countText.text=count.ToString();
+	}
 
-    public void ShowCountText(int count)
-    {
-        countText.text = count.ToString();
-    }
+	public void ShowCountText(int count) { countText.text=count.ToString(); }
 }
--- Assets/Scripts/ViewComponents/FailView.cs
+++ Assets/Scripts/ViewComponents/FailView.cs
@@ -4,54 +4,42 @@ using UnityEngine;
 
 public class FailView : BaseView
 {
-    public override void InitView()
-    {
+	public override void InitView() { }
 
-    }
+	public override void Start() { }
 
-    public override void Start()
-    {
+	public override void Update() { }
 
-    }
+	public override void HideView() { base.HideView(); }
 
-    public override void Update()
-    {
+	public void Close()
+	{
+		AudioManager.instance.btnSound.Play();
+		HideView();
+		GameManager.Instance.livesManager.ConsumeLife();
+		GameManager.Instance.BackToHome();
+	}
 
-    }
+	public void ResumeByAds()
+	{
+		AudioManager.instance.btnSound.Play();
+		GameManager.Instance.uiManager.gameView.ResumeTimer();
+		HideView();
+	}
 
-    public override void HideView()
-    {
-        base.HideView();
-    }
+	public void ResumeByCoin()
+	{
+		AudioManager.instance.btnSound.Play();
+		if(GameManager.Instance.currentCoin>=400)
+		{
+			GameManager.Instance.uiManager.gameView.ResumeTimer();
+			HideView();
+			GameManager.Instance.AddCoin(-400);
+		}
+		else
+		{
+			GameManager.Instance.uiManager.storeView.ShowView();
+		}
 
-    public void Close()
-    {
-        AudioManager.instance.btnSound.Play();
-        HideView();
-        GameManager.Instance.livesManager.ConsumeLife();
-        GameManager.Instance.BackToHome();
-    }
-
-    public void ResumeByAds()
-    {
-        AudioManager.instance.btnSound.Play();
-        GameManager.Instance.uiManager.gameView.ResumeTimer();
-        HideView();
-    }
-
-    public void ResumeByCoin()
-    {
-        AudioManager.instance.btnSound.Play();
-        if (GameManager.Instance.currentCoin >= 400)
-        {
-            GameManager.Instance.uiManager.gameView.ResumeTimer();
-            HideView();
-            GameManager.Instance.AddCoin(-400);
-        }
-        else
-        {
-            GameManager.Instance.uiManager.storeView.ShowView();
-        }
-
-    }
+	}
 }
